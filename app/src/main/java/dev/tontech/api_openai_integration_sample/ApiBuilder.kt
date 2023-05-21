57d7a706fab30886620aab8b8560684ca267408c
BrooitsFeiskJR <luizantonio0125@gmail.com>
2023-05-21 15:33:23 -0300

    Created Api Builder

---
+++ app/src/main/java/dev/tontech/api_openai_integration_sample/ApiBuilder.kt
@@ -0,0 +1,34 @@
+package dev.tontech.api_openai_integration_sample
+
+import com.aallam.openai.api.BetaOpenAI
+import com.aallam.openai.api.chat.ChatCompletionChunk
+import com.aallam.openai.api.chat.ChatCompletionRequest
+import com.aallam.openai.api.chat.ChatMessage
+import com.aallam.openai.api.chat.ChatRole
+import com.aallam.openai.api.http.Timeout
+import com.aallam.openai.api.model.ModelId
+import com.aallam.openai.client.OpenAI
+import com.aallam.openai.client.OpenAIConfig
+import kotlinx.coroutines.flow.Flow
+import kotlin.time.Duration.Companion.seconds
+
+class ApiBuilder {
+    private val apikey = "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"
+    private val config = OpenAIConfig(token = apikey, timeout = Timeout(socket = 60.seconds))
+    private val openAiApi = OpenAI(config)
+
+    @OptIn(BetaOpenAI::class)
+    fun sendMessageRequestToApi(message: String): Flow<ChatCompletionChunk> {
+        val chatCompletionRequest = ChatCompletionRequest(
+            model = ModelId("gpt-3.5-turbo"),
+            messages = listOf(
+                ChatMessage(
+                    role = ChatRole.User,
+                    content = message
+                )
+            )
+        )
+
+        return openAiApi.chatCompletions(chatCompletionRequest)
+    }
+}
